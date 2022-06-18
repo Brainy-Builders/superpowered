7ca1956882568d11722998be0a408fdb2c836cb5
Chloe Chiu <chloechiu2009@gmail.com>
2022-06-18 09:54:34 -0700

    new gyroturno testing

--- main.py
+++ main.py
@@ -6,3 +6,14 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile, Font
+from gyroturno import *
+
+def gyrotest():
+    gyro.reset_angle(360)
+    gyroturno(90)
+    gyro.reset_angle(-720)
+    gyroturno(-90)
+    gyro.reset_angle(405)
+    gyroturno(90)
+    robot.stop()
+gyrotest()
