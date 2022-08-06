752ff71b6d8891f3bf3e4a5441ee158e99506390
criddle858 <criddle858@gmail.com>
2022-08-06 10:51:37 -0700

    committing changes

--- tv.py
+++ tv.py
@@ -1,27 +1,16 @@
-#!/usr/bin/env pybricks-micropython
-#i commented -trey
-import time 
-import math 
+from common import *
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile, Font
 from gyroturno import *
 from pidlinefollow import *
-import common
+from linefollow import *
+from line_a_line import *
 
-import menu
-
-#gyrotest()
-
-
-#IF YOU FOUND THIS YOU ARE dumb
-#😎
-
-
-
-
-
-
-
+def flip_tv():
+    ev3.speaker.beep()
+    robot.drive(100, 0)
+    time.sleep(4)
+    robot.stop()
 
