9008ab947a9ab786def62efd72aa19f0e30744b2
criddle858 <criddle858@gmail.com>
2022-08-11 10:24:45 -0700

    line_follow back to trey & jackson version

--- linefollow.py
+++ linefollow.py
@@ -22,55 +22,54 @@ from common import *
 # An example of this code is
 # line_follow(followlength=200, followspeed=100, "left")
 # followlength=0 goes until an intersection
-def line_follow(length, speed, sensor, side, find_cross = False):
-    Kp = 0.25  #  the Constant 'K' for the 'p' proportional controller
-    Kd = 0.2   #  the Constant 'K' for the 'd' derivative term
-    Ki = 0.008 #  the Constand 'K' for the 'i' integratl term
-
-    # Work out how far to go first
+def line_follow(length, speed, sensor, side, find_cross = False, gain_mod=1.0):
     go_distance = robot.distance() + length
     # Calculate the light threshold. Choose values based on your measurements.
+    threshold = (BLACK + WHITE) / 2
     ev3.screen.print("WHITE", WHITE)
     ev3.screen.print("BLACK", BLACK)
-    threshold = (BLACK + WHITE) / 2
-
-    DRIVE_SPEED = speed*30/200
-    
-    # Configure side_polarity based on side of line to follow
-    side_polarity = 1
-    if side.lower() == "right":
-        side_polarity = -1
-    
+    DRIVE_SPEED = speed
+    # For example, if the light value deviates from the threshold by 10, the robot
+    # steers at 10*1.2 = 12 degrees per second.
+    if side.lower() == "left":
+        PROPORTIONAL_GAIN = 0.67 * gain_mod
+    else:
+        PROPORTIONAL_GAIN = -0.67 * gain_mod
     print(sensor, "r" in sensor.lower())
-    
     if "r" in sensor.lower():
         follow_sensor = right_colorsensor
         detection_sensor = left_colorsensor
     else:
         follow_sensor = left_colorsensor 
         detection_sensor = right_colorsensor
-    
-    integral = [0]  # initialize
+    Ki = 0. #  the Constanbricks.ev3devices import (
+    #Motot 'K' for the 'i' integral term
+    integral = [Ki]
     lastError = [0] # initialize
-    
+    Kd = 0 #  the Constant 'K' for the 'd' derivative term
     def apply_corrections():
-        error = follow_sensor.reflection()-threshold
-        # initialize
-        Tp = DRIVE_SPEED
+        error = follow_sensor.reflection()-50 # proportional
+        
+        Kp =  PROPORTIONAL_GAIN#  the Constant 'K' for the 'p' proportional controller
+        
+            # initialize
+        Tp = speed
         if (error == 0):
             integral[0] = 0
         else:
             integral[0] = integral[0] + error 
-        derivative = error - lastError[0]
-        
-        correction = (Kp*(error) + Ki*(integral[0]) + Kd*derivative) * side_polarity
-        power_left = Tp + correction
-        power_right = Tp - correction   
-        left_wheel.dc(power_left) 
-        right_wheel.dc(power_right) 
-        
-        lastError[0] = error  
-        # print(str(Kp) + "," + str(Kd) + "," + str(Ki) + "," + "error " + str(error) + "; correction " + str(correction)  + "; integral " + str(integral)  + "; derivative " + str(derivative))   
+            derivative = error - lastError[0]
+            
+            correction = (Kp*(error) + Ki*(integral[0]) + Kd*derivative) * -1
+            robot.drive(Tp, correction/-1)
+            power_left = Tp + correction
+            power_right = Tp - correction   
+            
+            #left_wheel.dc(power_left) 
+            #right_wheel.dc(power_right) 
+            
+            lastError[0] = error  
+            print("error " + str(error) + "; correction " + str(correction)  + "; integral " + str(integral)  + "; derivative " + str(derivative)+ "; power_left " + str(power_left) + "; power_right " + str(power_right))   
 
     while robot.distance() < go_distance:
       apply_corrections()
@@ -78,10 +77,12 @@ def line_follow(length, speed, sensor, side, find_cross = False):
     if find_cross == True:
         while get_color(detection_sensor) != Color.WHITE:
             apply_corrections()
+            #ev3.screen.print(detection_sensor.reflection())
         ev3.screen.print("found white")
         while get_color(detection_sensor) != Color.BLACK:
             apply_corrections()
-        ev3.screen.print("found black")        
+        #ev3.screen.print(detection_sensor.reflection())
+        ev3.screen.print("found black")  
             
 def test1():
     time.sleep(5)
