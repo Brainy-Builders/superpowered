e4668b119836bc7fe5f001b648d8843bfcf1c937
Chloe Chiu <chloechiu2009@gmail.com>
2022-09-14 17:36:54 -0700

    collect

--- collectit.py
+++ collectit.py
@@ -2,28 +2,28 @@ from common import *
 import linefollow
 from gyroturno import *
 from gyrostraight import *
+from pidlinefollow import *
 def main():
-        gyro.reset_angle(0)
-        forward_dist(100,0,180)
-        ev3.speaker.beep(duration=25)
-        while(get_color(left_colorsensor) != Color.BLACK):
-                robot.drive(100,0)
-        robot.turn(30)
-        robot.stop()
-        pidline(sensor='left', distance=350, speed=200, Kp=0.2, Ki=0.001, Kd=0.3, find_cross = False)
-        while(get_color(right_colorsensor) != Color.WHITE):
-                robot.drive(50,0)
-        while(get_color(right_colorsensor) != Color.BLACK):
-                robot.drive(50,0)
-
-        robot.stop()
-        # gyro.reset_angle(0)
-        # gyro_straight(400, 200)
-        # linefollow.line_follow(length=350,speed=150,sensor="left",side="left",find_cross=True)
+        travel()
+        collectyessir()
+def travel():
+    gyro.reset_angle(0)
+    forward_dist(100,0,180)
+    while(get_color(left_colorsensor) != Color.BLACK):
+        robot.drive(100,0)
+    robot.turn(30)
+    robot.stop()
+#     pidline(sensor='left', distance=350, speed=200, Kp=0.2, Ki=0.001, Kd=0.3, find_cross = False)
+#     while(get_color(right_colorsensor) != Color.WHITE):
+#         robot.drive(50,0)
+#     while(get_color(right_colorsensor) != Color.BLACK):
+#         robot.drive(50,0)
+#     robot.stop()
+def collectyessir():
         forward_dist(200, 0, 200)
         gyroturno(90)
         forward_dist(100, 0, 50)
-        move_motor(300, 700)
+        # move_motor(300, 700)
         # linefollow.line_follow(length=450,speed=150,sensor="right",side="left")
         linefollow.line_follow(200, 200, "right", "left")
         robot.stop()
