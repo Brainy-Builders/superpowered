836cf1cb401eb432a6baacf55d79ac9973389945
jdszekeres <amyc21@yahoo.com>
2022-09-24 12:07:26 -0700

    get to water

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from pybricks.media.ev3dev import SoundFile, ImageFile, Font
 from gyroturno import *
 
 import common
+from common import *
 import traceback
 gyro.reset_angle(0)
 ev3.screen.draw_image(x = 0, y = 0, source = "splash.png", transparent=None)
@@ -20,4 +21,4 @@ ev3.screen.draw_image(x = 0, y = 0, source = "splash.png", transparent=None)
 import menu
 
 #IF YOU FOUND THIS YOU SHOULD MINE CRYPTO
-#😎
+
