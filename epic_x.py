b1732e707a4ddd969445f0dfe9416d80b962148a
jdszekeres <amyc21@yahoo.com>
2022-10-21 17:43:19 -0700

    fhj.rewrhmnfdsrh

--- epic_x.py
+++ epic_x.py
@@ -2,12 +2,13 @@ from common import *
 from gyroturno import gyroturno,gyro_stop
 import time
 def main():
-    forward_distance(400,0,450,.5)
+    forward_distance(400,0,400,.5)
     robot.stop()
     forward_dist(300,35,450)
     forward_distance(300, 0, 100, 0)
     robot.stop()
-    gyroturno(-25)
+    gyro.reset_angle(0)
+    gyroturno(-90)
     gyro_stop()
     robot.drive(700,10)
     time.sleep(10)
