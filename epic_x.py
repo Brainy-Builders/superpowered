d2ce3ade83641f3aa6d8711bd98712941bc69280
Chloe Chiu <chloechiu2009@gmail.com>
2022-10-22 10:49:53 -0700

    last mission

--- epic_x.py
+++ epic_x.py
@@ -3,12 +3,13 @@ from gyroturno import gyroturno,gyro_stop
 import time
 def main():
     forward_distance(400,0,450,.5)
-    robot.stop()
-    forward_dist(300,35,410)
-    robot.stop()
+    # robot.stop()
+    forward_dist(300,32,450)
+    # robot.stop()
     gyroturno(-20)
-    gyro_stop()
-    robot.drive(700,3)
-    time.sleep(10)
+    forward_distance(700, 3, 700, t_prime=1)
+    # gyro_stop()
+    # robot.drive(700,3)
+    # time.sleep(10)
     robot.stop()
 
