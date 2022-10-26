f2f052095f42f1ebef7a03451d0884d7cf511109
criddle858 <criddle858@gmail.com>
2022-10-26 11:00:01 -0700

    deleted commented stuff

--- main.py
+++ main.py
@@ -1,24 +1,11 @@
 #!/usr/bin/env pybricks-micropython
-#i commented -trey
-# import time
-# import math 
-# import os
-# from pybricks.parameters impo
-# rt Port, Stop, Direction, Button, Color
-# from pybricks.tools import 
-# wait, StopWatch, DataLog
-# from pybricks.robotics import DriveBase
-# from pybricks.media.ev3dev import SoundFile, ImageFile, Font
-# speed up launch
 from gyroturno import *
-
 import common
 from common import *
+
 import traceback
 gyro.reset_angle(0)
 ev3.screen.draw_image(x = 0, y = 0, source = "splash.png", transparent=None)
-# os.popen("""beep -f  -l 300 -n -f 824 -l $((300 * 3/2)) -n -f 980 -l $((300/2)) -n -f 873 -l 300 -n -f 824 -l $((300 * 2)) -n -f 1234 -l 300 -n -f 1100 -l $((300*5/2)) -n -f 925 -l $((300*5/2)) -n -f 824 -l $((300*3/2)) -n -f 980 -l $((300/2)) -n -f 873 -l 300 -n -f 777 -l $((300*2)) -n -f 873 -l 300 -n -f 617 -l $((300*5/2))""")
-#time.sleep(5)
 
 # The "import menu" below runs the menu program!
 import menu
