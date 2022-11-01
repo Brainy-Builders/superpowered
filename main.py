f34bc2679e01960b4623a8695530926fd0417372
criddle858 <criddle858@gmail.com>
2022-11-01 08:32:19 -0700

    Miles forgot

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import common
 from common import *
 
 import traceback
-gyro.reset_angle(0)
 ev3.screen.draw_image(x = 0, y = 0, source = "splash.png", transparent=None)
 
 # The "import menu" below runs the menu program!
