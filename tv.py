be017fe41bdb66fd8aefd92e7f87e06bdca823b1
Chloe Chiu <chloechiu2009@gmail.com>
2022-11-02 16:02:37 -0700

    tv changes fr

--- tv.py
+++ tv.py
@@ -25,17 +25,19 @@ def flip_tv():
     halftesla2()
     
 def windthing(): #go to wind#
-    gyro_straight(150, -100)
+    gyro_straight(125, -100)
     gyroturno(-45+5-2)
     forward_dist(speed = 400, turn_rate = 1, distance = 300, t_prime=1) # slight turn right to not miss white line
     # ev3.speaker.beep(25)
+    foundblack = False
     turn_right = False
     max_distance = robot.distance()
     while turn_right == False:
       robot.drive(100,0)
-      if right_colorsensor.reflection() < 25:
+      if right_colorsensor.reflection() < 15:
         turn_right = True
-      if robot.distance() > max_distance+100:
+        foundblack = True
+      if robot.distance() > max_distance+125:
         turn_right = True
     # while get_color(right_colorsensor) != Color.WHITE:  
     #     robot.drive(50, 0)                              
@@ -45,7 +47,8 @@ def windthing(): #go to wind#
     #     robot.drive(50, 0)
     # robot.stop()
     # ev3.speaker.beep(25) 
-    forward_dist(speed = 50, turn_rate = 0, distance = 50) # go slightly past white
+    if foundblack:
+        forward_dist(speed = 50, turn_rate = 0, distance = 50) # go slightly past white
     gyroturno(45)
     forward_dist(speed = 250, turn_rate = 0, distance = 85)
     
@@ -62,49 +65,14 @@ def windthing(): #go to wind#
         time.sleep(0.2)
     robot.stop()
 
-def halftesla():
-    # back_motor.run_until_stalled(speed=100, then=Stop.HOLD, duty_limit=None)
-    gyro.reset_angle(45)
-    # forward_dist(-200, 0, -140)
-    gyro_straight(distance=130, speed=-200)
-    gyroturno(137)
-    # robot.stop()
-    robot.drive(-200,-5) # slight angle into the car model
-    time.sleep(1.5)
-    robot.stop()
-    # gyro.reset_angle(135)  # uncomment if/when reliably aligned
-    # forward_dist(-200, 0, -300)
-    back_motor.run_time(speed = -200, time=800)  # down
-    forward_dist(speed=375, turn_rate=0, distance=100)
-    forward_dist(speed=375, turn_rate=0, distance=50) # extra distance to allow car to fall
-    gyro_stop()
-    time.sleep(0.5)
-    back_motor.run_time(speed = 200, time = 700, wait=False) # up
-    gyroturno(135+82, rate_control=0.5)
-    robot.stop()
-    forward_dist(100, 0, 140)
-
-    gyroturno(-225, rate_control=0.7)  # move forward here
-    gyro_straight(700, 500)
-    # robot.turn(-70)       # most of the turn
-    # robot.drive(200,-100) # rest of the turn while moving forward to keep energy unit
-    # time.sleep(0.5)
-    # robot.drive(300,0)    # get the rest of the way home
-
-    # time.sleep(2)
-    robot.stop()
-    # Try this: 
-    # back_motor.run_until_stalled(speed=100, then=Stop.HOLD, duty_limit=None)
-    # back_motor.run_until_stalled(100, stop_type=Stop.HOLD)
-
 def halftesla2():
     # backup into car
-    forward_dist(speed=-250, turn_rate=0, distance=-120, t_prime = 0.5)
+    forward_dist(speed=-250, turn_rate=20, distance=-150, t_prime = 0.5)
     gyroturno(120)
     # ev3.speaker.beep()
     # time.sleep(3)
-    robot.drive(speed=-250,turn_rate=0) 
-    time.sleep(0.75)
+    robot.drive(speed=-200,turn_rate=0) 
+    time.sleep(0.5)
     back_motor.run_time(speed = -200, time=1000, wait=False)  # down
     time.sleep(0.75)
     gyro_stop()
@@ -112,9 +80,10 @@ def halftesla2():
     # time.sleep(3)
     # Let car down
     #Changed dist from 230 to 260:
-    forward_dist(speed=200, turn_rate=0, distance=260, t_prime=0.5)
+    forward_dist(speed=200, turn_rate=0, distance=200)
     gyro_stop()
-    back_motor.run_time(speed = 400, time = 1000, wait=False) # up
+    back_motor.run_time(speed = 500, time = 900, wait=False)
+    forward_dist(200, 0, 60) # up
     # ev3.speaker.beep()
     # time.sleep(3)
     # push the car away, then run into Toy Factory
