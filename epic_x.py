f341a5fe721407b0ce19c8b60c22b5b6ffdbf0b5
Chloe Chiu <chloechiu2009@gmail.com>
2022-11-18 16:18:25 -0800

    last

--- epic_x.py
+++ epic_x.py
@@ -5,13 +5,14 @@ def main():
     forward_distance(400,0,350,.5)
     # robot.stop()
     forward_dist(300,27,500)
-    forward_angle(300, -180, -60)
+    forward_angle(250, -180, -50)
     # ev3.speaker.beep()
     # robot.stop()
     # gyroturno(-20)
     forward_distance(700, 2, 1000, t_prime=1)
     # gyro_stop()
-    # robot.drive(700,3)
+    # robot.drive(700,3)20
+    
     # time.sleep(10)
     robot.stop()
 
