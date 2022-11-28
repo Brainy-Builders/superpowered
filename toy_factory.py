2e915f8a5baa8fd9f0ceaedcdf7e421de8a797a1
criddle858 <criddle858@gmail.com>
2022-11-28 15:35:18 -0800

    Toy comments

--- toy_factory.py
+++ toy_factory.py
@@ -19,33 +19,19 @@ from common import *
 from pidlinefollow import *
 from gyrostraight import *
 from gyroturno import *
-def toy():
-    # gyro.reset_angle(0)
-    # forward_distance(400, 0, 26, 1)
-    # time.sleep(1)
-    # gyroturno(-62)
-    # forward_distance(400, 0, 250, 1)
-    # robot.drive(200, 0)
-    # time.sleep(1.5)
-    # robot.stop()
-    # move_motor(400, 900)
-    # time.sleep(.5)
-    # forward_distance(-800, 0, -80, 1)
-    # robot.drive(-800, 80)
-    # time.sleep(1)
-    # robot.stop()
-    toy_alternative()
-    
 
-def toy_alternative():
+
+def toy():
+    #Get to toy factory and allign
     gyro_straight(distance=290, speed=400, reset_angle=None, t_prime = 1)
-    ev3.speaker.beep()
     robot.drive(300, 0)
     time.sleep(.7)
     robot.drive(200,0)
     time.sleep(0.3)
     gyro_stop()
+    # wait for units to drop into factory
     time.sleep(.25)
+    # get home
     forward_distance(-400, 0, -150, 0)
     robot.drive(-400, 0)
     time.sleep(1)
