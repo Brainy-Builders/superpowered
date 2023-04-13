e71022f7b4de81b256f96f4f9fb3718d7b53c5ad
jdszekeres <jdszekeres@gmail.com>
2023-04-12 19:23:12 -0700

    callum

--- power.py
+++ power.py
@@ -21,28 +21,30 @@ def get_there_and_dispense():
     time.sleep(.8)
     gyro_stop()
     #dispense units
-    main_motor.run_time(speed=60*16, time=1500)
-    main_motor.run_time(speed=-90*16, time=1500)
+    main_motor.run_time(speed=60*16, time=1700)
+    main_motor.run_time(speed=-90*16, time=1800)
 
 def get_off_wall():
     forward_distance(-250, 0, -70)
     gyroturn(angle=35, rate_control=.8, speed=-250)
     robot.reset()
     # time.sleep(.1)
-    gyroturn(angle=-30, rate_control=1, speed=350, stop=False)
+    gyroturn(angle=-30, rate_control=1, speed=400, stop=False)
 
 def get_home():
     dist = robot.distance()
-    forward_dist(speed=300, turn_rate=0, distance=470-dist)
-    gyroturno(angle=30, rate_control=1, speed=300, stop=False)
+    #forward_dist(speed=300, turn_rate=0, distance=470-dist) old code, saved in case of emergency
+    forward_dist(speed=500, turn_rate=0, distance=430-dist)
+    gyroturno(angle=30, rate_control=1, speed=400, stop=False)
     ev3.speaker.beep(duration=25)
-    gyroturno(angle=0, rate_control=0.35, speed=350, stop=False)
+    gyroturno(angle=0, rate_control=0.35, speed=400, stop=False)
     ev3.speaker.beep(duration=25)
     # forward_angle(speed=500, turn_rate=70, angle= 50)
     # ev3.speaker.beep()
     # forward_angle(speed=500, turn_rate=-30, angle=-45)
     # ev3.speaker.beep()
     # forward_distance(speed=500, turn_rate=-5, distance=300)
-    robot.drive(speed=350, turn_rate=-5)
-    time.sleep(0.5)
+    #robot.drive(speed=350, turn_rate=-5) old code, saved in case of emergency
+    robot.drive(speed=400, turn_rate=-15)
+    time.sleep(0.3)
     gyro_stop()
